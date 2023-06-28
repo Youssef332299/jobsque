0ee5afdcd9e3d6f14f1dfb4b436d006036d2f7b9
yousse <youssefayman329@gmail.com>
2023-06-28 17:51:31 +0300

    youssef_28_6

--- lib/screens/auth/widgets/work/provider/workProvider.dart
+++ lib/screens/auth/widgets/work/provider/workProvider.dart
@@ -1,29 +1,19 @@
 import 'package:flutter/cupertino.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'workState.dart';
+import 'package:jobsque/screens/auth/widgets/work/provider/workState.dart';
 
 
   //  work_______________________________________
 
-  // Future<void> loadIconRecent() async {
-  //   SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-  //   List<bool?> works = [];
-  //   for (int i = 0; i < 6; i++) {
-  //     bool? work = sharedPreferences.getBool('work$i');
-  //     works.add(work ?? false);
-  //   }
-  //   state.work = works;
-  //   notifyListeners();
-  // }
-  // workSelected(isSelected){
-  //   if (isSelected == true) {
-  //     isSelected = false;
-  //     notifyListeners();
-  //   } else {
-  //     isSelected = true;
-  //     notifyListeners();
-  //   }
-  // }
+class WorkProvider extends ChangeNotifier{
+  WorkState state = WorkState();
 
-
+  void selectCategory(Map<String, dynamic> item) {
+    if (!state.worksSelected.contains(item)) {
+      state.worksSelected.add(item);
+    } else {
+      state.worksSelected.remove(item);
+    }
+    notifyListeners();
+  }
+}
 
--- lib/screens/auth/widgets/work/provider/workState.dart
+++ lib/screens/auth/widgets/work/provider/workState.dart
@@ -1,3 +1,34 @@
 import 'package:flutter/cupertino.dart';
 import 'package:iconsax/iconsax.dart';
 
+
+class WorkState {
+
+  List<Map<String, dynamic>> work = [
+    {"icon": const Icon(Iconsax.bezier4), "name": "UI/UX Designer"},
+    {"icon": const Icon(Iconsax.pen_tool4), "name": "Illustrator Designer"},
+    {"icon": const Icon(Iconsax.code4), "name": "Developer"},
+    {"icon": const Icon(Iconsax.graph4), "name": "Management"},
+    {"icon": const Icon(Iconsax.graph4), "name": "Management"},
+    {"icon": const Icon(Iconsax.monitor_mobbile4), "name": "Information Technology"},
+    {"icon": const Icon(Iconsax.cloud_add4), "name": "Research and Analytics"}
+  ];
+  List<Map<String, dynamic>> worksSelected = [];
+
+  bool isSelected13 = false;
+  bool isSelected12 = false;
+  bool isSelected11 = false;
+  bool isSelected10 = false;
+  bool isSelected9 = false;
+  bool isSelected8 = false;
+  bool isSelected7 = false;
+  bool isSelected6 = false;
+  bool isSelected5 = false;
+  bool isSelected4 = false;
+  bool isSelected3 = false;
+  bool isSelected2 = false;
+  bool isSelected = false;
+}
+
+
+}
